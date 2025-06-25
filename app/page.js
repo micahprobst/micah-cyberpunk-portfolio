f2459044296426396f959d4e089f1ef5ce9dfebd
micahprobst <micah.x.probst@gmail.com>
2025-06-24 22:31:52 -0600

    changed some colors

--- app/page.js
+++ app/page.js
@@ -609,8 +609,8 @@ export default function Home() {
                     )}
                     
                     {/* Corner accents */}
-                    <div className="absolute top-0 left-0 w-4 h-4 border-l-2 border-t-2 border-cyan-400 group-hover:border-purple-400 transition-colors"></div>
-                    <div className="absolute bottom-0 right-0 w-4 h-4 border-r-2 border-b-2 border-cyan-400 group-hover:border-purple-400 transition-colors"></div>
+                    <div className="absolute top-0 left-0 w-4 h-4 border-l-2 border-t-2 border-cyan-400 group-hover:border-pink-400 transition-colors"></div>
+                    <div className="absolute bottom-0 right-0 w-4 h-4 border-r-2 border-b-2 border-cyan-400 group-hover:border-pink-400 transition-colors"></div>
                   </div>
                 </div>
               </div>
