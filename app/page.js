7815ad83cf5ec71ca64ffd52a9e5340f1acb1147
micahprobst <micah.x.probst@gmail.com>
2025-07-05 16:43:07 -0600

    update certificate name

--- app/page.js
+++ app/page.js
@@ -49,7 +49,7 @@ export default function Home() {
       { name: "Small Business Ownership", institution: "Front Range Community College", image: "/certificates/small-business-ownership.JPG", inProgress: false }
     ],
     projectManagement: [
-      { name: "Project Management Fundamentals", institution: "Google", image: null, inProgress: true },
+      { name: "Foundations of Project Management", institution: "Google", image: null, inProgress: true },
       { name: "AI Product Manager Specialization", institution: "Microsoft", image: null, inProgress: true },
       { name: "AI Product Management Specialization", institution: "Duke", image: null, inProgress: true }
     ],
